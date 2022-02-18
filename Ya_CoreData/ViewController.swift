4e22bf0d94e0ba8bedc6b36c04114fe33c4d722e
macsonm <75022784+macsonm@users.noreply.github.com>
2022-02-18 13:34:54 +0300

    Initial Commit

---
+++ Ya_CoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ya_CoreData
+//
+//  Created by Maxim M on 18.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
