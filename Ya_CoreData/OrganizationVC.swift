88dcb17badd4be3a6f68461dfab416815fd9d78d
macsonm <75022784+macsonm@users.noreply.github.com>
2022-03-04 14:20:32 +0300

    NSFetchRequest чтение из БД данных нет обновления списка при добавлении новой организации

--- Ya_CoreData/OrganizationVC.swift
+++ Ya_CoreData/OrganizationVC.swift
@@ -11,7 +11,7 @@ class OrganizationVC: UITableViewController {
         guard let context = context else { return }
 
         guard let organization = NSEntityDescription.insertNewObject(forEntityName: "Organization", into: context) as? Organization else { return }
-        organization.name = "Yandex"
+        organization.name = "test"
 
         do{
             try context.save()
@@ -19,14 +19,22 @@ class OrganizationVC: UITableViewController {
             print(error)
         }
         
-        
-        
     }
+    
+    fileprivate let cellIdentifier = "OrganizationCellIdentifier"
+    fileprivate var organizations = [Organization]()
     
     var context: NSManagedObjectContext! {
         didSet {
-            tView.refreshControl?.endRefreshing()       
+            fetchData()
         }
+    }
+    
+    func fetchData() {
+        let request = NSFetchRequest<Organization>(entityName: "Organization")      //получение данных из CoreData
+        organizations = try! context.fetch(request)
+        tView.reloadData()
+        tView.refreshControl?.endRefreshing()
     }
     
     
@@ -37,4 +45,24 @@ class OrganizationVC: UITableViewController {
         tView.refreshControl?.beginRefreshing()
     }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return organizations.count
+    }
+    
+    override func tableView(_ tableView: UITableView,cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath )
+        let organization = organizations[indexPath.row]
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = organization.name
+        content.secondaryText = "Employees \(organization.employee?.accessibilityElementCount() ?? 0)"      //????
+        
+        cell.contentConfiguration = content
+        
+        return cell
+    }
+    
 }
+
+
+    
